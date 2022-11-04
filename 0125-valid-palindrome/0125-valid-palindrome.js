d8f1ddd1b0ed924c8229c5098cd6948e469a0ece
Ryan Mullen <65626651+Rymul@users.noreply.github.com>
2022-11-04 11:13:49 -0700

    Time: 123 ms (51.71%), Space: 46.4 MB (46.30%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,7 +4,5 @@
  */
 var isPalindrome = function(s) {
     let replaced = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    console.log(replaced)
-    console.log(replaced.split("").reverse().join(""))
     return replaced === replaced.split("").reverse().join("");
 };
